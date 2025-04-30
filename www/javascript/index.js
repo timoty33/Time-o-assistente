41539632fba0bd4d7f01a692c0e52be8517f1777
timoty33 <timotyduo@gmail.com>
2025-04-29 22:27:52 -0300

    Interface pronta

--- www/javascript/index.js
+++ www/javascript/index.js
@@ -1,13 +1,16 @@
 function trocarPlay() {
   const iconPlay = document.getElementById("iconPlay");
   const circle = document.getElementById("circle");
+  const status = document.getElementById("status");
 
   if (iconPlay.classList.contains("fa-play")) {
     iconPlay.classList.replace("fa-play", "fa-pause");
     circle.classList.add("active");
+    status.textContent = "Ativado";
   } else {
     iconPlay.classList.replace("fa-pause", "fa-play");
     circle.classList.remove("active");
+    status.textContent = "Desativado";
   }
 }
 
